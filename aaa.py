f996857fe57518befba2f6abf203a88b10930fe6
Asierjunquera1 <ajunquerabartolome@gmail.com>
2023-05-08 18:32:17 +0200

    apartado 4

--- aaa.py
+++ aaa.py
@@ -33,4 +33,26 @@ def alojamientos_con_mas_plazas(alojamientos, numero_ocupantes):
         if int(alojamientos[i]["bedrooms"])>=numero_ocupantes:
             lista_alojamientos.append(alojamientos[i])
 
+def alojamientos_mas_baratos_por_distrito(alojamientos, distrito):
+    alojamientos_del_distrito=[]
+    for i in range(len(lista_alojamientos())):
+        if lista_alojamientos()[i]["host_neighbourhood"]==distrito:
+            alojamientos_del_distrito.append(lista_alojamientos[i])
+    
+    precios=[]
+    for i in range(len(alojamientos_del_distrito)):
+        precios.append(alojamientos_del_distrito[i]["price"])
+    precios_ordenados=precios.sort()
+    lista_alojamientos=[]
+    if len(precios_ordenados)<=10:
+        for i in range(len(precios_ordenados)):
+            for j in range(len(alojamientos_del_distrito)):
+                if precios_ordenados[i]==alojamientos_del_distrito[j]["price"]:
+                    lista_alojamientos.append(alojamientos_del_distrito[j])
+    else:
+        for i in range(0, 10):
+            for j in range(len(alojamientos_del_distrito)):
+                if precios_ordenados[i]==alojamientos_del_distrito[j]["price"]:
+                    lista_alojamientos.append(alojamientos_del_distrito[j])
+
 
